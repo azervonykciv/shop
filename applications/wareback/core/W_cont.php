a4d1ce8fb4be18728a38e7d44fe040d1f3ad6dd3
Vicky Novreza <vnovreza@gmail.com>
2016-12-01 15:45:39 -0800

    Front End Warehouse System

--- applications/wareback/core/W_cont.php
+++ applications/wareback/core/W_cont.php
@@ -3,6 +3,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class W_cont extends CI_Controller
 {
+
+	protected $table;
+	protected $view;
+	protected $
+
+
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model('crud_model');
+	}
 	public function get($data)
 	{
 		
