55e30919ac7e79b8a9279e0307423f3694f0aa20
gustialfian <gustialfianmp@gmail.com>
2016-12-18 00:51:02 +0000

    add assets home page

---
+++ applications/distributor/views/pages/home_view.php
@@ -0,0 +1,195 @@
+<!DOCTYPE HTML>
+<!--
+	Projection by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Projection by TEMPLATED</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="<?php echo base_url().'assets/projection/css/main.css' ?>" />
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+				<div class="inner">
+					<a href="index.html" class="logo"><strong>Projection</strong> by TEMPLATED</a>
+					<!-- <nav id="nav">
+						<a href="index.html">Home</a>
+						<a href="generic.html">Generic</a>
+						<a href="elements.html">Elements</a>
+					</nav> -->
+					<a href="#navPanel" class="navPanelToggle"><span class="fa fa-bars"></span></a>
+				</div>
+			</header>
+
+		<!-- Banner -->
+			<section id="banner">
+				<div class="inner">
+					<header>
+						<h1>Welcome to API Distributtor</h1>
+					</header>
+
+					<div class="flex ">
+
+						<div>
+							<span class="icon fa-user"></span>
+							<h3>Siapa</h3>
+							<p>Suspendisse amet ullamco</p>
+						</div>
+
+						<div>
+							<span class="icon fa-server"></span>
+							<h3>Apa</h3>
+							<p>Class aptent taciti ad litora</p>
+						</div>
+
+						<div>
+							<span class="icon fa-question"></span>
+							<h3>Berapa</h3>
+							<p>Nulla vitae mauris non felis</p>
+						</div>
+
+					</div>
+
+					<footer>
+						<!-- <a href="#" class="button">Get Started</a> -->
+					</footer>
+				</div>
+			</section>
+
+
+		<!-- Three -->
+			<section id="three" class="wrapper align-center">
+				<div class="inner">
+					<div class="flex flex-2">
+						<article>
+							<header>
+								<h3>Free</h3>
+							</header>
+							<p>
+                Morbi in sem quis dui placerat ornare. Pellentesquenisi<br />
+                euismod in, pharetra a, ultricies in diam sed arcu. Cras<br />
+                consequat  egestas augue vulputate.
+              </p>
+							<footer>
+								<a href="#" class="button" data-toggle="modal" data-target="#free-modal">Learn More</a>
+							</footer>
+						</article>
+
+						<article>
+							<header>
+								<h3>Premium</h3>
+							</header>
+							<p>
+                Pellentesque fermentum dolor. Aliquam quam lectus<br />
+                facilisis auctor, ultrices ut, elementum vulputate, nunc<br />
+                blandit ellenste egestagus commodo.
+              </p>
+							<footer>
+								<a href="#" class="button" data-toggle="modal" data-target="#premium-modal">Learn More</a>
+							</footer>
+						</article>
+					</div>
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+
+					<h3>Get in touch</h3>
+
+					<form action="#" method="post">
+
+						<div class="field half first">
+							<label for="name">Name</label>
+							<input name="name" id="name" type="text" placeholder="Name">
+						</div>
+						<div class="field half">
+							<label for="email">Email</label>
+							<input name="email" id="email" type="email" placeholder="Email">
+						</div>
+						<div class="field">
+							<label for="message">Message</label>
+							<textarea name="message" id="message" rows="6" placeholder="Message"></textarea>
+						</div>
+						<ul class="actions">
+							<li><input value="Send Message" class="button alt" type="submit"></li>
+						</ul>
+					</form>
+
+					<div class="copyright">
+						&copy; Untitled. Design:
+            <a href="https://templated.co">TEMPLATED</a>.
+            Images: <a href="https://unsplash.com">Unsplash</a>.
+					</div>
+
+				</div>
+			</footer>
+
+      <div id="free-modal" class="modal fade" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button> -->
+              <h4 class="modal-title">Free</h4>
+            </div>
+            <div class="modal-body">
+              <form>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Merk</label>
+                  <select class="" name="">
+                    <option value=""></option>
+                  </select>
+                </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="button" data-dismiss="modal">Close</button>
+              <button type="button" class="button">Save changes</button>
+            </div>
+          </form>
+          </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+      </div><!-- /.modal -->
+
+      <div id="premium-modal" class="modal fade" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button> -->
+              <h4 class="modal-title">Premium</h4>
+            </div>
+            <div class="modal-body">
+              <form>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Merk</label>
+                  <select class="" name="">
+                    <option value=""></option>
+                  </select>
+                </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="button" data-dismiss="modal">Close</button>
+              <button type="button" class="button">Save changes</button>
+            </div>
+          </form>
+          </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+      </div><!-- /.modal -->
+
+		<!-- Scripts -->
+      <!-- jQuery 2.2.3 -->
+      <script src="<?php echo base_url() ?>assets/plugins/jQuery/jquery-2.2.3.min.js"></script>
+      <!-- Bootstrap 3.3.6 -->
+      <script src="<?php echo base_url() ?>assets/bootstrap/js/bootstrap.min.js"></script>
+			<script src="<?php echo base_url().'assets/projection/js/skel.min.js' ?>"></script>
+			<script src="<?php echo base_url().'assets/projection/js/util.js' ?>"></script>
+			<script src="<?php echo base_url().'assets/projection/js/main.js' ?>"></script>
+
+	</body>
+</html>
