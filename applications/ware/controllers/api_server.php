5f53575d7c0442369b1491719de1bcbe0633b75a
Vicky Novreza <vnovreza@gmail.com>
2016-12-19 22:20:37 -0800

    API Packet Selector done + Ware project Structure

--- applications/ware/controllers/api_server.php
+++ applications/ware/controllers/api_server.php
@@ -1,5 +1,4 @@
 <?php
-
 include(APPPATH.'libraries/REST_Controller.php');
 class api_server extends REST_Controller
 {
@@ -10,43 +9,39 @@ class api_server extends REST_Controller
         $this->load->model('crud_model');
     }
 
+
+//For get barang by request
     public function index_get()
     {
+      $idf = $this->get('idf');
+// 01 standard req - 02 spesific req
+      if($idf == "01")
+      {
+          $tab = $this->get('tab');
+          $id_user = $this->get('id_user');
+          $kat1 = $this->get('kat1');
+          $kat2 = $this->get('kat2');
+
+          $identifier = $this->crud_model->match('packages','ID_User',$id_user)->row();
+          $barang = $this->crud_model->match($tab,$kat1,$kat2,$identifier->jml_brng)->result();
+          if($barang){
+              $this->response($barang,200);
+          }
+      }elseif($idf == "02")
+      {
         $tab = $this->get('tab');
-        $id_user = $this->get('id_user');
-        $kat1 = $this->get('kat1');
-        $kat2 = $this->get('kat2');
-
-        $identifier = $this->crud_model->match('packages','ID_User',$id_user)->row();
-
-        /**echo "paket gratisan";
-        echo "<pre>";
-        print_r($tab);
-        print_r($id_user);
-        print_r($kat1);
-        print_r($kat2);
-        print_r($cond);
-        print_r($identifier->jml_brng);
-        die();**/
-        $barang = $this->crud_model->match($tab,$kat1,$kat2,$identifier->jml_brng)->result();
-        if($barang){
-            $this->response($barang,200);
-        }
-
+        $
+      }
     }
 
     public function index_put()
     {
         $id2 = $this->put('id1');
-        $barang = $this->crud_model->match('barang','KODE_BARANG',$id2,1);
-        //$stok = $barang->QTY - 1;
-        //$data['QTY'] = $stok;
+        $barang = $this->crud_model->match('barang','KODE_BARANG',$id2)->row();
+        $data['QTY'] = $barang->QTY - 1;
 
-        print_r($barang);
 
-        die;
-
-       // $update = $this->crud_model->update('barang',$data,'KODE_BARANG',$id2);
+       $update = $this->crud_model->update('barang',$data,'KODE_BARANG',$id2);
         //if($update)
         //{
          //   $this->response($data,200);
--- applications/ware/views/frontBase.php
+++ applications/ware/views/frontBase.php
@@ -13,20 +13,20 @@
 <meta name="description" content="Archer - Responsive Landing Page">
 
 <!--Favicon-->
-<link rel="apple-touch-icon" sizes="57x57" href="<?php echo base_url('asset/front/img/favicons/apple-touch-icon-57x57.png') ?>">
-<link rel="apple-touch-icon" sizes="60x60" href="<?php echo base_url('asset/front/img/favicons/apple-touch-icon-60x60.png') ?>">
-<link rel="apple-touch-icon" sizes="72x72" href="<?php echo base_url('asset/front/img/favicons/apple-touch-icon-72x72.png') ?>">
-<link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url('asset/front/img/favicons/apple-touch-icon-76x76.png') ?>">
-<link rel="apple-touch-icon" sizes="114x114" href="<?php echo base_url('asset/front/img/favicons/apple-touch-icon-114x114.png') ?>">
-<link rel="apple-touch-icon" sizes="120x120" href="<?php echo base_url('asset/front/img/favicons/apple-touch-icon-120x120.png') ?>">
-<link rel="apple-touch-icon" sizes="144x144" href="<?php echo base_url('asset/front/img/favicons/apple-touch-icon-144x144.png') ?>">
-<link rel="apple-touch-icon" sizes="152x152" href="<?php echo base_url('asset/front/img/favicons/apple-touch-icon-152x152.png') ?>">
-<link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url('asset/front/img/favicons/apple-touch-icon-180x180.png') ?>">
-<link rel="icon" type="image/png" href="<?php echo base_url('asset/front/img/favicons/favicon-32x32.png') ?>" sizes="32x32">
-<link rel="icon" type="image/png" href="<?php echo base_url('asset/front/img/favicons/android-chrome-192x192.png') ?>" sizes="192x192">
-<link rel="icon" type="image/png" href="<?php echo base_url('asset/front/img/favicons/favicon-96x96.png') ?>" sizes="96x96">
-<link rel="icon" type="image/png" href="<?php echo base_url('asset/front/img/favicons/favicon-16x16.png') ?>" sizes="16x16">
-<link rel="manifest" href="<?php echo base_url('asset/front/img/favicons/manifest.json') ?>">
+<link rel="apple-touch-icon" sizes="57x57" href="<?php echo base_url('assets/front/img/favicons/apple-touch-icon-57x57.png') ?>">
+<link rel="apple-touch-icon" sizes="60x60" href="<?php echo base_url('assets/front/img/favicons/apple-touch-icon-60x60.png') ?>">
+<link rel="apple-touch-icon" sizes="72x72" href="<?php echo base_url('assets/front/img/favicons/apple-touch-icon-72x72.png') ?>">
+<link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url('assets/front/img/favicons/apple-touch-icon-76x76.png') ?>">
+<link rel="apple-touch-icon" sizes="114x114" href="<?php echo base_url('assets/front/img/favicons/apple-touch-icon-114x114.png') ?>">
+<link rel="apple-touch-icon" sizes="120x120" href="<?php echo base_url('assets/front/img/favicons/apple-touch-icon-120x120.png') ?>">
+<link rel="apple-touch-icon" sizes="144x144" href="<?php echo base_url('assets/front/img/favicons/apple-touch-icon-144x144.png') ?>">
+<link rel="apple-touch-icon" sizes="152x152" href="<?php echo base_url('assets/front/img/favicons/apple-touch-icon-152x152.png') ?>">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url('assets/front/img/favicons/apple-touch-icon-180x180.png') ?>">
+<link rel="icon" type="image/png" href="<?php echo base_url('assets/front/img/favicons/favicon-32x32.png') ?>" sizes="32x32">
+<link rel="icon" type="image/png" href="<?php echo base_url('assets/front/img/favicons/android-chrome-192x192.png') ?>" sizes="192x192">
+<link rel="icon" type="image/png" href="<?php echo base_url('assets/front/img/favicons/favicon-96x96.png') ?>" sizes="96x96">
+<link rel="icon" type="image/png" href="<?php echo base_url('assets/front/img/favicons/favicon-16x16.png') ?>" sizes="16x16">
+<link rel="manifest" href="<?php echo base_url('assets/front/img/favicons/manifest.json') ?>">
 <link rel="shortcut icon" href="img/favicons/favicon.ico">
 <meta name="msapplication-TileColor" content="#2b5797">
 <meta name="msapplication-TileImage" content="img/favicons/mstile-144x144.png">
@@ -34,11 +34,11 @@
 <meta name="theme-color" content="#ffffff">
 
 <!--Favicon end-->
-<link href="<?php echo base_url('asset/front/css/bootstrap.min.css') ?>" rel="stylesheet">
-<link href="<?php echo base_url('asset/front/css/font-awesome.min.css') ?>" rel="stylesheet">
-<link href="<?php echo base_url('asset/front/css/magnific-popup.css') ?>" rel="stylesheet">
-<link href="<?php echo base_url('asset/front/css/owl.carousel.css') ?>" rel="stylesheet">
-<link href="<?php echo base_url('asset/front/css/main.css') ?>" rel="stylesheet">
+<link href="<?php echo base_url('assets/front/css/bootstrap.min.css') ?>" rel="stylesheet">
+<link href="<?php echo base_url('assets/front/css/font-awesome.min.css') ?>" rel="stylesheet">
+<link href="<?php echo base_url('assets/front/css/magnific-popup.css') ?>" rel="stylesheet">
+<link href="<?php echo base_url('assets/front/css/owl.carousel.css') ?>" rel="stylesheet">
+<link href="<?php echo base_url('assets/front/css/main.css') ?>" rel="stylesheet">
 </head>
 
 <?php if($this->session->flashdata('eReg')) :?>
@@ -62,30 +62,28 @@
 <?php endif ?>
 
 
-<!--hero section-->
-
-<?php echo $contents; ?>
+<?php echo $contents ?>
 
 
 
 <!--contact form modal end-->
 
-<script src="<?php echo base_url('asset/front/js/jquery-2.2.1.min.js') ?>"></script>
-<script src="<?php echo base_url('asset/front/js/bootstrap.min.js') ?>"></script>
-<script src="<?php echo base_url('asset/front/js/jquery.magnific-popup.min.js') ?>"></script>
-<script src="<?php echo base_url('asset/front/js/owl.carousel.min.js') ?>"></script>
-<script src="<?php echo base_url('asset/front/js/jquery.waypoints.min.js') ?>"></script>
-<script src="<?php echo base_url('asset/front/js/jquery.animateNumber.min.js') ?>"></script>
-<script src="<?php echo base_url('asset/front/js/jquery.ajaxchimp.min.js') ?>"></script>
-<script src="<?php echo base_url('asset/front/js/tweetie.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/front/js/jquery-2.2.1.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/front/js/bootstrap.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/front/js/jquery.magnific-popup.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/front/js/owl.carousel.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/front/js/jquery.waypoints.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/front/js/jquery.animateNumber.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/front/js/jquery.ajaxchimp.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/front/js/tweetie.min.js') ?>"></script>
 <!--[if IE 9]>
 <script src="js/placeholders.min.js"></script>
 <![endif]-->
-<script src="<?php echo base_url('asset/front/js/main.js') ?>"></script>
+<script src="<?php echo base_url('assets/front/js/main.js') ?>"></script>
 <script async src="../../../../platform.twitter.com/widgets.js"></script>
 <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
-<script src="<?php echo base_url('asset/front/js/gmap.js') ?>"></script>
-<script src="<?php echo base_url('asset/front/js/retina.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/front/js/gmap.js') ?>"></script>
+<script src="<?php echo base_url('assets/front/js/retina.min.js') ?>"></script>
 </body>
 
 <script type="text/javascript">
